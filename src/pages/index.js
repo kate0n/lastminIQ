46d803c3ade93e1784e99db6a51fc3914d8d951d
Kate <mikhailina.ekaterina@gmail.com>
2020-07-30 14:02:49 +0300

    add force

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,31 @@ const IndexPage = props => {
   const [facebookID, setFacebookID] = useQueryParam("id", NumberParam)
   // console.log("QUERY PARAMETER", facebookID)
 
+  function openInBrowser(target, browserScheme) {
+    var ifc = document.createElement("div")
+    ifc.innerHTML = `<iframe src='${browserScheme}${target}' style='width:0;height:0;border:0; border:none;visibility: hidden;'></iframe>`
+    document.body.appendChild(ifc)
+  }
+
+  function isInApp(appSpecificUserAgents) {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera
+    for (i = 0; i <= appSpecificUserAgents.length; i++) {
+      if (userAgent.indexOf(appSpecificUserAgents[i]) > -1) return true
+    }
+  }
+
   React.useEffect(() => {
+    ;(function tryOpenBrowser() {
+      if (document.body) {
+        if (isInApp(["FBAN", "FBAV"])) {
+          //https://iq.lastmin.tv/id?=facebookID
+          openInBrowser(window.location.href, "googlechrome://navigate?url=")
+        }
+      } else {
+        window.requestAnimationFrame(tryOpenBrowser)
+      }
+    })()
+
     fetch(`${state.url}/localize/main.json`)
       .then(response => response.json())
       .then(mainJson => mainJson)
